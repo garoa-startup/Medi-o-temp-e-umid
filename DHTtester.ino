00f9c3c73d1811e00fc24fd99f6d762438835498
Brendo-C137 <102158779+Brendo-C137@users.noreply.github.com>
2022-05-15 14:55:27 -0300

    Add files via upload

---
+++ DHTtester.ino
@@ -0,0 +1,31 @@
+#include "DHT.h"
+
+#define DHTPIN D3
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("DHT11 test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  Serial.print(F("Humidade: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperatura: "));
+  Serial.print(t);
+  Serial.print(F("°C \n"));
+  
+}
